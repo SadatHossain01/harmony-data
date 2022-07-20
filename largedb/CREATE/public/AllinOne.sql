ee67a8ba02c270ad110f5e00e28a7a98703a6e76
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-07-20 09:52:43 +0600

    removed the unnecessary postPartOf table and updated the ERD

--- largedb/CREATE/public/AllinOne.sql
+++ largedb/CREATE/public/AllinOne.sql
@@ -514,23 +514,3 @@ COMMENT ON TABLE "HasVoted" IS 'many-to-many relation between user and poll';
 ALTER TABLE "HasVoted"
     OWNER TO postgres;
 
-CREATE TABLE IF NOT EXISTS "PostPartOf" (
-    post_id  INTEGER NOT NULL
-        CONSTRAINT postpartof_post_post_id_fk
-            REFERENCES "Post"
-            ON UPDATE CASCADE ON DELETE CASCADE,
-    group_id INTEGER NOT NULL
-        CONSTRAINT postpartof_group_group_id_fk
-            REFERENCES "Group"
-            ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT postpartof_pk
-        PRIMARY KEY (post_id, group_id),
-    CONSTRAINT postpartof_pk_2
-        UNIQUE (post_id, group_id)
-);
-
-COMMENT ON TABLE "PostPartOf" IS 'to know which group this post is of';
-
-ALTER TABLE "PostPartOf"
-    OWNER TO postgres;
-
