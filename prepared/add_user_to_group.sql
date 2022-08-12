21fa5ed9b30d839c31491099ad81b4610f6f6c06
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-13 00:38:47 +0600

    Revert "added cse 19 peeps to database"
    This reverts commit bf1c6251aefc7b4f98df2a6b75431694325b825a.

--- prepared/add_user_to_group.sql
+++ prepared/add_user_to_group.sql
@@ -5,9 +5,4 @@
 INSERT INTO "MemberOf" (member_id, group_id)
 VALUES ($1::INT4, $2::INT4);
 
--- PREPARE add_user_to_group_by_name(VARCHAR, VARCHAR) AS
---     INSERT INTO "MemberOf" (member_id, group_id)
---     VALUES ((SELECT user_id FROM "User" WHERE user_name = $1::VARCHAR),
---             (SELECT group_id FROM "Group" WHERE group_name = $2::VARCHAR));
--- EXECUTE add_user_to_group_by_name('risenfromashes', 'EBG');
-
+-- EXECUTE add_user_to_group('Sadat999', 'CSE19');
