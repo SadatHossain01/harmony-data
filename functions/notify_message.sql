5524d6f29cc3325711afb24ea9b9f666260e86e0
risenfromashes <ashrafurkhan37@gmail.com>
2022-08-26 23:53:00 +0600

    add dp_link in notify_message

--- functions/notify_message.sql
+++ functions/notify_message.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE FUNCTION notify_message() RETURNS TRIGGER AS $$
 DECLARE
   sender VARCHAR;
+  sender_dp TEXT;
   payload JSON;
 BEGIN
 
-  SELECT user_name INTO sender 
-  FROM "User"
+  SELECT user_name, c.link INTO sender, sender_dp
+  FROM "User" u
+  LEFT JOIN "Content" c
+  ON u.dp_id = c.content_id
   WHERE user_id = NEW.sender_id;
 
   IF NEW.group_id IS NOT NULL THEN
@@ -13,6 +16,7 @@ BEGIN
       'id', NEW.message_id::text,
       'sender_id', NEW.sender_id::text, 
       'sender_name', sender,
+      'sender_dp', sender_dp,
       'content', NEW.m_text,
       'time', NEW.time::VARCHAR);
     PERFORM pg_notify('chat/group/'|| NEW.group_id, payload::text);
